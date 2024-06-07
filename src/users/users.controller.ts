8f70063da53c9991958d1d8566e56117060ca791
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-06-07 15:52:37 +0200

    Edit paths to the files

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+  UseGuards,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { AdminAuthGuard } from 'src/auth/admin-auth.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get('/')
+  getAllUsers(): any {
+    return this.usersService.getAllUsers();
+  }
+
+  @Get('/:id')
+  async getUserById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const user = await this.usersService.getUsersById(id);
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  @Delete(':id')
+  @UseGuards(AdminAuthGuard)
+  @UseGuards(JwtAuthGuard)
+  public async delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.usersService.getUsersById(id)))
+      throw new NotFoundException('User not found');
+    await this.usersService.deleteUserById(id);
+    return { success: true };
+  }
+}
