23ac6ebca20b4d10c2a46a48523d870a19abacd6
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-06-07 15:59:00 +0200

    Change reviews module

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReviewsController } from './reviews.controller';
 import { ReviewsService } from './reviews.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [ReviewsController],
-  providers: [ReviewsService]
+  providers: [ReviewsService],
+  imports: [PrismaModule],
 })
 export class ReviewsModule {}
