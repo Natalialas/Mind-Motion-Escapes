ea1a0861ccc0a994c9cc8a79af29dae8d71b3d8b
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-06-13 12:21:23 +0200

    Add endpoints for categories

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { Category, Tour } from '@prisma/client';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Get('/')
+  async getAll(): Promise<Category[]> {
+    return this.categoriesService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id') id: string): Promise<Category> {
+    const category = await this.categoriesService.getById(id);
+    if (!category) throw new NotFoundException('Category not found');
+    return category;
+  }
+
+  @Get('/:id/tours')
+  async getToursByCategoryId(@Param('id') id: string): Promise<Tour[]> {
+    const tours = await this.categoriesService.getToursByCategoryId(id);
+    return tours;
+  }
+}
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/shared/services/prisma.service';
+import { Category, Tour } from '@prisma/client';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<Category[]> {
+    return this.prismaService.category.findMany();
+  }
+
+  public async getById(id: string): Promise<Category> {
+    const category = await this.prismaService.category.findUnique({
+      where: { id },
+    });
+    if (!category) throw new NotFoundException('Category not found');
+    return category;
+  }
+
+  public async getToursByCategoryId(categoryId: string): Promise<Tour[]> {
+    return this.prismaService.category
+      .findUnique({
+        where: { id: categoryId },
+      })
+      .tours();
+  }
+}
