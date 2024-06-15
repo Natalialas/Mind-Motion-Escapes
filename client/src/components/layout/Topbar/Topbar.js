1f824928a5e93b9d5ba1e822d8959c2da781f6ab
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-06-15 13:04:02 +0200

    Change layout of topbar

--- client/src/components/layout/Topbar/Topbar.js
+++ client/src/components/layout/Topbar/Topbar.js
@@ -1,28 +1,33 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 import styles from './TopBar.module.scss';
 
 const TopBar = () => {
   return (
     <Navbar expand="lg" className={styles.topbar}>
-      <Container>
-        <Navbar.Brand href="#home" className={styles.logo} style={{ color: 'rgb(249,115,0)' }}>
+      <Container className={styles.container}>
+        <div className={styles.left} style={{ color: 'rgb(249,115,0)', fontSize: '1.2rem' }}>
+          <Link to="/adventures" className={styles.link}>
+            Adventures
+          </Link>
+          <Link to="/about" className={styles.link}>
+            About us
+          </Link>
+        </div>
+        <Navbar.Brand href="#home" className={styles.logo} style={{ color: 'rgb(249,115,0)', fontSize: '1.5rem' }}>
           Mind&Motion Escapes
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className={`${styles.navItems} ml-auto`} >
-            <Nav.Link href="#home" className={styles.link}>Home</Nav.Link>
-            <Nav.Link href="#adventures" className={styles.link}>Adventures</Nav.Link>
-            <Nav.Link href="#about" className={styles.link}>About us</Nav.Link>
-            <Nav.Link href="#login" className={styles.link}>Login</Nav.Link>
-            <Nav.Link href="#cart" className={styles.link}>
-              <FontAwesomeIcon icon={faShoppingCart} />
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <div className={styles.right} style={{ color: 'rgb(249,115,0)', fontSize: '1.2rem' }}>
+          <Link to="/login" className={styles.link}>
+            Login
+          </Link>
+          <Link to="/cart" className={styles.link}>
+            <FontAwesomeIcon icon={faShoppingCart} />
+          </Link>
+        </div>
       </Container>
     </Navbar>
   );
