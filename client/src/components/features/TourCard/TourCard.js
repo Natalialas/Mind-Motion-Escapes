9e5e76f716ccabf4f22917da1fc06711ef0de31c
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-06-18 10:09:22 +0200

    Change format of date

--- client/src/components/features/TourCard/TourCard.js
+++ client/src/components/features/TourCard/TourCard.js
@@ -3,13 +3,16 @@ import { Card } from 'react-bootstrap';
 import styles from './TourCard.module.scss';
 
 const TourCard = ({ tour }) => {
+  const date = new Date(tour.date);
+  const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+
   return (
     <Card className={styles.tourCard}>
       <Card.Img variant="top" src={`/images/${tour.photo}`} className={styles.cardImage} />
       <Card.Body>
         <Card.Title>{tour.name}</Card.Title>
         <Card.Text>{tour.location}</Card.Text>
-        <Card.Text>{tour.date}</Card.Text>
+        <Card.Text>{formattedDate}</Card.Text>
       </Card.Body>
     </Card>
   );
