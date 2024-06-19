5d9e5aa4683ef1fa660c1bbc3cd488be66697001
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-06-19 14:17:52 +0200

    Add files

--- client/src/components/layout/Topbar/Topbar.js
+++ client/src/components/layout/Topbar/Topbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
