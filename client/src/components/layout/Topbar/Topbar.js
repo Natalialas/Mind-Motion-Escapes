1384808b7eef040d26c6fffa71462eee23abed77
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-06-25 09:58:54 +0200

    Add collapsible menu

--- client/src/components/layout/Topbar/Topbar.js
+++ client/src/components/layout/Topbar/Topbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { NavLink, useNavigate } from 'react-router-dom';
@@ -9,30 +9,43 @@ const TopBar = () => {
   const navigate = useNavigate();
 
   const handleCartClick = () => {
-    navigate(`/cartitems`);
+    navigate('/cartitems');
   };
 
   return (
     <Navbar expand="lg" className={styles.topbar}>
       <Container className={styles.container}>
-        <div className={styles.left} style={{ color: 'rgb(249,115,0)', fontSize: '1.2rem' }}>
-          <NavLink to="/adventures" className={styles.link} activeClassName={styles.active}>
-            Adventures
-          </NavLink>
-          <NavLink to="/about" className={styles.link} activeClassName={styles.active}>
-            About us
-          </NavLink>
+        <div className={styles.left}>
+          <Navbar.Brand as={NavLink} to="/" className={`${styles.logo} ${styles.link}`} style={{ color: 'rgb(249,115,0)', fontSize: '1.5rem' }}>
+            Mind&Motion Escapes
+          </Navbar.Brand>
         </div>
-        <Navbar.Brand as={NavLink} to="/" className={`${styles.logo} ${styles.link}`} style={{ color: 'rgb(249,115,0)', fontSize: '1.5rem' }}>
-          Mind&Motion Escapes
-        </Navbar.Brand>
-        <div className={styles.right} style={{ color: 'rgb(249,115,0)', fontSize: '1.2rem' }}>
-          <NavLink to="/login" className={styles.link} activeClassName={styles.active}>
-            Login
-          </NavLink>
-          <div className={styles.link} onClick={handleCartClick} style={{ cursor: 'pointer' }}>
-            <FontAwesomeIcon icon={faShoppingCart} />
-          </div>
+        <div className={styles.right}>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className={`${styles.navbarNav} ml-auto`}>
+              <Nav.Item>
+                <Nav.Link as={NavLink} to="/adventures" className={styles.link} activeClassName={styles.active} style={{ color: 'rgb(249,115,0)', fontSize: '1.2rem' }}>
+                  Adventures
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={NavLink} to="/about" className={styles.link} activeClassName={styles.active} style={{ color: 'rgb(249,115,0)', fontSize: '1.2rem' }}>
+                  About us
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={NavLink} to="/login" className={styles.link} activeClassName={styles.active} style={{ color: 'rgb(249,115,0)', fontSize: '1.2rem' }}>
+                  Login
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item style={{ color: 'rgb(249,115,0)', fontSize: '1.2rem' }}>
+                <div className={`${styles.link} ${styles.icon}`} onClick={handleCartClick} style={{ cursor: 'pointer' }}>
+                  <FontAwesomeIcon icon={faShoppingCart} />
+                </div>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
         </div>
       </Container>
     </Navbar>
