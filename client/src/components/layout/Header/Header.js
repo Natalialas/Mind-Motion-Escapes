e730a3100d629534e6d6e30c1088d56a949c1a32
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-06-25 10:39:09 +0200

    Add responsive styles for header

--- client/src/components/layout/Header/Header.js
+++ client/src/components/layout/Header/Header.js
@@ -6,8 +6,8 @@ const Header = () => {
     <header className={styles.header} style={{ backgroundImage: `url(/images/header.jpg)` }}>
       <div className={styles.overlay}>
         <h1 className={styles.heading}>
-          <span className={styles.line1}>Learn to</span>
-          <span className={styles.line2}>surf with us!</span>
+          <span className={styles.line1}>LEARN TO</span>
+          <span className={styles.line2}>SURF WITH US!</span>
         </h1>
         <h2>Hawaii, Waikiki Beach</h2>
         <p>Starting from 1,689 $</p>
