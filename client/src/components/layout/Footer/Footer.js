523b08f08054ea8ee58f98e9b11955de319c9a60
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-06-26 13:56:07 +0200

    Change styles for footer

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -5,37 +5,39 @@ import styles from './Footer.module.scss';
 const Footer = () => (
   <section className={styles.blog}>
     <Container className={styles.container}>
-    <img src="/images/logo_transparent.png" alt="Logo" className={styles.logo} />
-    <div className={styles.content}>
-      <div className={styles.block}>
-        <h5 className={styles.blockTitle}>LINKS</h5>
-        <button>Legal & Privacy</button>
-        <button>Contact</button>
-        <button>Gift card</button>
-        <button>Customer service</button>
+      <div className={styles.content}>
+        <img src="/images/logo_transparent.png" alt="Logo" className={styles.logo} />
+        <div className={styles.blocks}>
+          <div className={styles.block}>
+            <h5 className={styles.blockTitle}>INFO</h5>
+            <ul>
+              <li><button>About us</button></li>
+              <li><button>Contact</button></li>
+              <li><button>Gift card</button></li>
+              <li><button>Customer service</button></li>
+            </ul>
+          </div>
+          <div className={styles.block}>
+            <h5 className={styles.blockTitle}>ACCOUNT</h5>
+            <ul>
+              <li><button>My profile</button></li>
+              <li><button>Order history</button></li>
+              <li><button>Wishlist</button></li>
+              <li><button>Shopping bag</button></li>
+            </ul>
+          </div>
+          <div className={styles.block}>
+            <h5 className={styles.blockTitle}>SOCIALS</h5>
+            <ul>
+              <li><button>Instagram</button></li>
+              <li><button>LinkedIn</button></li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className={styles.block}>
-        <h5 className={styles.blockTitle}>ACCOUNT</h5>
-        <button>My profile</button>
-        <button>My order history</button>
-        <button>My wishlist</button>
-        <button>My shopping bag</button>
+      <div className={styles.footerBottom}>
+        <p>© Mind&Motion Escapes 2024</p>
       </div>
-      <div className={styles.block}>
-        <h5 className={styles.blockTitle}>SOCIALS</h5>
-        <button>Instagram</button>
-        <button>LinkedIn</button>
-      </div>
-      <div className={styles.block}>
-        <h5 className={styles.blockTitle}>COMPANY</h5>
-        <button>About us</button>
-        <button>Blog</button>
-        <button>Contact Us</button>
-      </div>
-    </div>
-    <div className="text-center">
-      <p>Copyright © 2024</p>
-    </div>
     </Container>
   </section>
 );
